84e02581a31b57003f53a1aee0a87e76f2ddff6d
Leg0shii <dragonsaremyfavourite@gmail.com>
2024-12-08 12:51:59 +0100

    add practice file

--- src/main/java/de/legoshi/AOC_2024/parser.py
+++ src/main/java/de/legoshi/AOC_2024/parser.py
@@ -3,6 +3,10 @@ import json
 import requests
 
 def get_content(day):
+    if day == "p":
+        f = open("test_data", "r")
+        return f.read()
+
     link = "https://adventofcode.com/2024/day/" + day + "/input"
 
     with open('session_file.json', 'r') as file:

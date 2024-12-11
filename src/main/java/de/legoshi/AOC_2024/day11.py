68071cc1660573902ebbb41530399fdb37bbeb90
Leg0shii <dragonsaremyfavourite@gmail.com>
2024-12-11 15:55:52 +0100

    AOC2024 day 11

--- src/main/java/de/legoshi/AOC_2024/day11.py
+++ src/main/java/de/legoshi/AOC_2024/day11.py
@@ -1,4 +1,47 @@
 import parser
 
+def add_to_dict(new_num, prev_num, remove=True):
+    if new_num in temp_dict:
+        temp_dict[new_num] += number_dict[prev_num]
+    else:  
+        temp_dict[new_num] = number_dict[prev_num]
+    
+
 content = parser.get_content('11')
-print(content)
+number_list = [int(x) for x in content.replace('\n', '').split(' ')]
+number_dict = {num: 1 for num in number_list}
+temp_dict = dict(number_dict)
+
+index = 0
+while index < 75:
+    index += 1
+    i = -1
+
+    temp_dict = dict()
+    to_do_list = list(number_dict.keys())
+    for num in to_do_list:
+        if num == 0:
+            add_to_dict(1, 0)
+            continue
+
+        str_number = str(num)
+        if len(str_number) % 2 == 0:
+            cut_off = int(len(str_number)/2)
+            first_num = int(str_number[:cut_off])
+            second_num = int(str_number[cut_off:])
+
+            add_to_dict(first_num, num, False)
+            add_to_dict(second_num, num, False)
+
+            continue
+
+        add_to_dict(num * 2024, num)
+    
+    number_dict = dict(temp_dict)
+
+
+result = 0
+for val in number_dict.values():
+    result += val
+    
+print(result)
--- src/main/java/de/legoshi/AOC_2024/parser.py
+++ src/main/java/de/legoshi/AOC_2024/parser.py
@@ -4,13 +4,15 @@ import json
 import requests
 
 def get_content(day):
-    if day == "p":
-        f = open("test_data", "r")
+    cript_dir = os.path.dirname(__file__)
+
+    if day == 'p':
+        practice_file_path = os.path.join(cript_dir, 'test_data')
+        f = open(practice_file_path, 'r')
         return f.read()
 
-    link = "https://adventofcode.com/2024/day/" + day + "/input"
+    link = 'https://adventofcode.com/2024/day/' + day + '/input'
 
-    cript_dir = os.path.dirname(__file__)
     session_file_path = os.path.join(cript_dir, 'session_file.json')
 
     with open(session_file_path, 'r') as file:
